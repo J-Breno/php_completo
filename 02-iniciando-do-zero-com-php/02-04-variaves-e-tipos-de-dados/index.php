757ede0ee065c920c0f7ef1bbb3601e964984a6f
J-Breno <joao.breno85@hotmail.com>
2025-08-28 16:34:16 -0300

    Variáveis e tipos de dados

--- 02-iniciando-do-zero-com-php/02-04-variaves-e-tipos-de-dados/index.php
+++ 02-iniciando-do-zero-com-php/02-04-variaves-e-tipos-de-dados/index.php
@@ -7,18 +7,42 @@ fullStackPHPClassName("02.04 - Variáveis e tipos de dados");
  * [ variáveis ] https://php.net/manual/pt_BR/language.variables.php
  */
 fullStackPHPClassSession("variáveis", __LINE__);
+$userFirstName = "João";
+$userLastName = "Breno";
+echo "<h3>{$userFirstName} {$userLastName}</h3>";
 
+$userAge = 20;
 
+echo "<p>{$userFirstName} {$userLastName} tem <span class='tag'>{$userAge}</span></p>";
 /**
  * [ tipo boleano ] true | false
  */
 fullStackPHPClassSession("tipo boleano", __LINE__);
+
+$true = true;
+$false = false;
+var_dump($true, $false);
+
+$bestAge = ($userAge > 30);
+var_dump($bestAge);
+
+$a = 0;
+$b = 0.0;
+$c = "";
+$d = [];
+$e = null;
+
+var_dump($a, $b, $c, $d, $e);
+
 
 /**
  * [ tipo callback ] call | closure
  */
 fullStackPHPClassSession("tipo callback", __LINE__);
 
+$code = "<article><h1>Um Call User Function!</h1></article>";
+$codeClear = call_user_func("strip_tags", $code);
+var_dump($codeClear);
 
 /**
  * [ outros tipos ] string | array | objeto | numérico | null
