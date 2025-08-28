e3c7edb235ff0da4524eed60bfe7074cf1183942
J-Breno <joao.breno85@hotmail.com>
2025-08-28 17:01:30 -0300

    Estruturas de controle

--- 02-iniciando-do-zero-com-php/02-07-estruturas-de-controle/index.php
+++ 02-iniciando-do-zero-com-php/02-07-estruturas-de-controle/index.php
@@ -9,15 +9,31 @@ fullStackPHPClassName("02.07 - Estruturas de controle");
  */
 fullStackPHPClassSession("if, elseif, else", __LINE__);
 
+$test = false;
+if($test){
+    var_dump(true);
+} else {
+    var_dump(false);
+}
 
-
+$rock = "AC/DC";
+if(!empty($rock)){
+    var_dump($rock);
+} else {
+    var_dump(false);
+}
 
 /*
  * [ isset ] https://php.net/manual/pt_BR/function.isset.php
  * [ empty] https://php.net/manual/pt_BR/function.empty.php
  */
 fullStackPHPClassSession("isset, empty, !", __LINE__);
-
+$rock = "";
+if(isset($rock)){
+    var_dump("Rock");
+} else {
+    var_dump("Die");
+}
 
 /*
  * [ switch ] https://secure.php.net/manual/pt_BR/control-structures.switch.php
