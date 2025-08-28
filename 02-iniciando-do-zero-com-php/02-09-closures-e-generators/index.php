bdfdaba2a32b7fea1fba9339f5de690c2bba4072
J-Breno <joao.breno85@hotmail.com>
2025-08-28 17:18:30 -0300

    Closures e generators

--- 02-iniciando-do-zero-com-php/02-09-closures-e-generators/index.php
+++ 02-iniciando-do-zero-com-php/02-09-closures-e-generators/index.php
@@ -7,9 +7,44 @@ fullStackPHPClassName("02.09 - Closures e generators");
  */
 fullStackPHPClassSession("closures", __LINE__);
  // funções anônimas
+$myAge = function($year) {
+    $age = date("Y") - $year;
+    return "<p>Você tem {$age} anos</p>";
+};
 
+echo $myAge(1900);
 
 /*
  * [ generators ] https://php.net/manual/pt_BR/language.generators.overview.php
  */
 fullStackPHPClassSession("generators", __LINE__);
+
+$iterator = 10;
+
+function showDate($days) {
+    $saveDate = [];
+    for($day = 1; $day <= $days; $day++) {
+        $saveDate[] = date("d/m/Y", strtotime("+{$days} days"));
+    }
+    return $saveDate;
+}
+
+echo "<div style='text-align: center'>";
+foreach (showDate(1) as $date) {
+    echo "<smal class='tag'>{$date}</small>".PHP_EOL;
+}
+echo "</div>";
+
+function generatorDate($days)
+{
+    for($day = 1; $day <= $days; $day++) {
+        yield date("d/m/Y", strtotime("+{$days} days"));
+    }
+}
+
+
+echo "<div style='text-align: center'>";
+foreach (generatorDate(1) as $date) {
+    echo "<smal class='tag'>{$date}</small>".PHP_EOL;
+}
+echo "</div>";
