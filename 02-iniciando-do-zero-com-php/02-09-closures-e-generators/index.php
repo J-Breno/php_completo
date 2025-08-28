98e8213379888c20cb3c0783aca709e0e6a8c3ba
J-Breno <joao.breno85@hotmail.com>
2025-08-28 16:07:24 -0300

    Iniciando o ambiente

---
+++ 02-iniciando-do-zero-com-php/02-09-closures-e-generators/index.php
@@ -0,0 +1,15 @@
+<?php
+require __DIR__ . '/../../fullstackphp/fsphp.php';
+fullStackPHPClassName("02.09 - Closures e generators");
+
+/*
+ * [ closures ] https://php.net/manual/pt_BR/functions.anonymous.php
+ */
+fullStackPHPClassSession("closures", __LINE__);
+ // funções anônimas
+
+
+/*
+ * [ generators ] https://php.net/manual/pt_BR/language.generators.overview.php
+ */
+fullStackPHPClassSession("generators", __LINE__);
---
+++ 02-iniciando-do-zero-com-php/02-10-requisicao-de-arquivos/index.php
@@ -0,0 +1,19 @@
+<?php
+require __DIR__ . '/../../fullstackphp/fsphp.php';
+fullStackPHPClassName("02.10 - Requisição de arquivos");
+
+/*
+ * [ include ] https://php.net/manual/pt_BR/function.include.php
+ * [ include_once ] https://php.net/manual/pt_BR/function.include-once.php
+ */
+fullStackPHPClassSession("include, include_once", __LINE__);
+// include não é obrigatório
+//include __DIR__."header.php"; __DIR__ = caminho absoluto
+
+
+/*
+ * [ require ] https://php.net/manual/pt_BR/function.require.php
+ * [ require_once ] https://php.net/manual/pt_BR/function.require-once.php
+ */
+fullStackPHPClassSession("require, require_once", __LINE__);
+// require é obrigatório
