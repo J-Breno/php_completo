b3e92f4cd98d4827c07cf604a1b4e3eceaccf19a
J-Breno <joao.breno85@hotmail.com>
2025-08-29 00:43:41 -0300

    Manipulação de objetos

--- 03-manipulacao-e-tratamento/03-04-manipulacao-de-objetos/index.php
+++ 03-manipulacao-e-tratamento/03-04-manipulacao-de-objetos/index.php
@@ -7,9 +7,24 @@ fullStackPHPClassName("03.04 - Manipulação de objetos");
  */
 fullStackPHPClassSession("manipulação", __LINE__);
 
+$arrProfile = [
+  "name" => "John Doe",
+  "email" => "john@doe.com",
+  "age" => 25,
+];
+$obj = (object)$arrProfile;
+
+echo "<p>{$obj->name}</p>}";
+$ceo = $obj;
+unset($ceo->age);
+var_dump($ceo);
 
 /**
  * [ análise ] class | objetcs | instances
  */
 fullStackPHPClassSession("análise", __LINE__);
 
+var_dump([
+    "class" => get_class($obj),
+    "methods" => get_class_methods($obj),
+]);
