cc0beb7e0daecb01af9d4502ef6594c6641416a2
J-Breno <joao.breno85@hotmail.com>
2025-08-29 04:14:48 -0300

    Manipulação de diretórios

--- 03-manipulacao-e-tratamento/03-08-gestao-de-diretorios/index.php
+++ 03-manipulacao-e-tratamento/03-08-gestao-de-diretorios/index.php
@@ -7,13 +7,25 @@ fullStackPHPClassName("03.08 - Gestão de diretórios");
  */
 fullStackPHPClassSession("verificar, criar e abrir", __LINE__);
 
+$folder = __DIR__ . "/uploads";
+if(!file_exists($folder) || !is_dir($folder)) {
+    mkdir($folder, 0755);
+} else {
+    var_dump(scandir($folder));
+}
 
 /*
  * [ copiar e renomear ] copy | rename
  */
 fullStackPHPClassSession("copiar e renomear", __LINE__);
 
-
+$file = __DIR__ . "/file.txt";
+var_dump(pathinfo($file), scandir($folder), scandir(__DIR__));
+if(!file_exists($file) || !is_file($file)) {
+    fopen($file, "w");
+} else {
+    copy($file, $folder . "/". basename($file));
+}
 /*
  * [ remover e deletar ] unlink | rmdir
  */
