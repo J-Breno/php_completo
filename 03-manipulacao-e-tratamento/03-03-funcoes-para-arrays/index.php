7c11bdea04306f26863eeee5fcfcb2fedd4bd881
J-Breno <joao.breno85@hotmail.com>
2025-08-29 00:12:30 -0300

    Funções para arrays

--- 03-manipulacao-e-tratamento/03-03-funcoes-para-arrays/index.php
+++ 03-manipulacao-e-tratamento/03-03-funcoes-para-arrays/index.php
@@ -7,18 +7,52 @@ fullStackPHPClassName("03.03 - Funções para arrays");
  */
 fullStackPHPClassSession("manipulação", __LINE__);
 
+$index = [
+  'AC/DC',
+    "Nirvana",
+    "Alter Bridge"
+];
 
+$assoc = [
+    "band_1" => 'AC/DC',
+    "band_2" => 'Nirvana',
+    "band_3" => 'Alter Bridge',
+];
+
+array_unshift($index,  "", "Peral Jam");
+array_push($index, "");
+
+array_shift($index);
+array_pop($index);
+
+$index = array_filter($index);
 /*
  * [ ordenação ] reverse | asort | ksort | sort
  */
 fullStackPHPClassSession("ordenação", __LINE__);
 
+$index = array_reverse($index);
 
+asort($index);
+ksort($index);
+krsort($index);
+sort($index);
+rsort($index);
 
 /*
  * [ verificação ]  keys | values | in | explode
  */
 fullStackPHPClassSession("verificação", __LINE__);
+
+var_dump([
+    array_keys($assoc),
+    array_values($assoc),
+]);
+
+if(in_array("AC/DC", $assoc)){
+    echo "<p>cadssssss</p>";
+}
+
 
 
 /**
