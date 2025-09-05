80ece1b7fd99139e25d236ac9f43f5fc634f1154
J-Breno <joao.breno85@hotmail.com>
2025-09-05 08:41:51 -0300

    Gestão de dependências

---
+++ 07-recursos-e-componentes/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jbrneo/fsphp',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e580a21d88523dcff689fd4919e694c1ed0423e1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jbrneo/fsphp' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e580a21d88523dcff689fd4919e694c1ed0423e1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
