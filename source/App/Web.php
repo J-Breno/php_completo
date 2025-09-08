a1159c6890e1c70eaa5a633d9b06735ac78271b4
J-Breno <joao.breno85@hotmail.com>
2025-09-08 06:01:42 -0300

    Erro de conexão e manutenção

--- source/App/Web.php
+++ source/App/Web.php
@@ -199,16 +199,35 @@ class Web extends Controller
     public function error(array $data): void
     {
         $error = new \stdClass();
-        $error->code = $data['errcode'];
-        $error->title = "Ooops. Conteúdo indisponível :/";
-        $error->message = "Sentimos muito, mas o conteúdo que você tentou acessar não existe, está indiponível no momento ou foi removido :/";
-        $error->linkTitle = "Continue navegando!";
-        $error->link = url_back();
+        switch ($data["errcode"]) {
+            case "problemas":
+                $error->code = "OPS";
+                $error->title = "Estamos enfrentando problemas!";
+                $error->message = "Parece que nosso serviço não está disponível no momento. Já estamos vendo isso, mas caso precise, envie um e-mail :)";
+                $error->linkTitle = "ENVIAR E-MAIL";
+                $error->link = "mailto:".CONF_MAIL_SUPPORT;
+                break;
+            case "manutencao":
+                $error->code = "OPS";
+                $error->title = "Descupe. Estamos em manutenção!";
+                $error->message = "Voltamos logo! Por hora estamos trabalhando para melhorar nosso conteúdo para você controlar melhor suas contas :P";
+                $error->linkTitle = null;
+                $error->link = null;
+                break;
+            default:
+                $error->code = $data['errcode'];
+                $error->title = "Ooops. Conteúdo indisponível :/";
+                $error->message = "Sentimos muito, mas o conteúdo que você tentou acessar não existe, está indiponível no momento ou foi removido :/";
+                $error->linkTitle = "Continue navegando!";
+                $error->link = url_back();
+                break;
+        }
+
 
         $head = $this->seo->render(
             "{$error->code} | {$error->title}",
             $error->message,
-            url_back("/ops/{$error->code}"),
+            url("/ops/{$error->code}"),
             theme("/assets/images/share.jpg"),
             false
         );
