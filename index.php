1086eea823e35ab45c3ec7440ff5238a6f221325
J-Breno <joao.breno85@hotmail.com>
2025-09-08 05:39:15 -0300

    Telas de login, cadastro e senha

--- index.php
+++ index.php
@@ -15,9 +15,16 @@ $route->namespace("Source\App");
 $route->get("/", "Web:home");
 $route->get("/sobre", "Web:about");
 $route->get("/termos", "Web:terms");
+
+// Blog
 $route->get("/blog", "Web:blog");
 $route->get("/blog/page/{page}", "Web:blog");
 $route->get("/blog/{postName}", "Web:blogPost");
+
+// auth
+$route->get("/entrar", "Web:login");
+$route->get("/recuperar", "Web:forget");
+$route->get("/cadastrar", "Web:register");
 
 $route->namespace("Source\App")->group("/ops");
 $route->get("/{errcode}", "Web:error");
--- source/App/Web.php
+++ source/App/Web.php
@@ -53,6 +53,10 @@ class Web extends Controller
         ]);
     }
 
+    /**
+     * @param array|null $data
+     * @return void
+     */
     public function blog(?array $data): void
     {
         $head = $this->seo->render(
@@ -71,6 +75,10 @@ class Web extends Controller
         ]);
     }
 
+    /**
+     * @param array $data
+     * @return void
+     */
     public function blogPost(array $data): void
     {
         $postName = $data["postName"];
@@ -85,6 +93,45 @@ class Web extends Controller
         echo $this->view->render("blog-post", [
             "head" => $head,
             "data" => $this->seo->data()
+        ]);
+    }
+
+    public function login(): void
+    {
+        $head = $this->seo->render(
+            "Entrar - ".CONF_SITE_NAME,
+            CONF_SITE_DESC,
+            url("/entrar"),
+            theme("/assets/images/share.jpg")
+        );
+        echo $this->view->render("auth-login", [
+            "head" => $head
+        ]);
+    }
+
+    public function forget(): void
+    {
+        $head = $this->seo->render(
+            "Recuperar Senha - ".CONF_SITE_NAME,
+            CONF_SITE_DESC,
+            url("/recuperar"),
+            theme("/assets/images/share.jpg")
+        );
+        echo $this->view->render("auth-forget", [
+            "head" => $head
+        ]);
+    }
+
+    public function register(): void
+    {
+        $head = $this->seo->render(
+            "Criar Conta - ".CONF_SITE_NAME,
+            CONF_SITE_DESC,
+            url("/cadastrar"),
+            theme("/assets/images/share.jpg")
+        );
+        echo $this->view->render("auth-register", [
+            "head" => $head
         ]);
     }
 
